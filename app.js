b29acd4a5de7691f81e2e181239f0b084b05ad7e
Tomy Sánchez <tomyysanchez03@gmail.com>
2024-10-01 18:27:13 -0300

    feat: Configuración de CORS y helmet

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
+import cors from 'cors';
 import express from 'express';
 import clientesRoutes from './routes/clientesRoutes.js';
 import dotenv from 'dotenv';
+import helmet from 'helmet';
 
 dotenv.config();
 
 const app = express();
 
+// MIDDLEWARES GLOBALES:
+app.use(
+  cors({
+    origin: 'http://localhost:1573'
+  })
+);
+app.use(helmet());
 app.use(express.json());
 
+// RUTAS:
 app.get('/', (req, res) => {
   res.send('Funcionando');
 });
-
-app.get('/api', (req, res) => {
-  res.send('Funcionando');
-});
-
 app.use('/api', clientesRoutes);
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}/`);
 });
